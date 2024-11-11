ae155ce214bb9bcf9003f604bcd133b99150f585
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-11 17:44:30 +0000

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num<0){
+        return "ERROR";
+    }
+    else if (num==0){
+        return '';
+    }
+    else{
+        let repeatedString = string;
+        for (i=0; i<num-1; i++){
+            repeatedString = repeatedString.concat(string)
+        }
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
