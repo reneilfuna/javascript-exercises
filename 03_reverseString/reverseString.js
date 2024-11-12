23270d30fbb102d1945edca6141cf93bd6401bfc
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-12 12:53:57 +0000

    Implement code to pass first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,18 +1,18 @@
 const reverseString = function(string) {
     //Function takes a string and then reverses the string.
-
+    const reversedString = [];
     // Loop through each character in the string
-    for (i=0; i<string.length-1; i++) {
-        
+    for (i=0; i<string.length; i++) {
+        // Read the character
+        char = string[i];
+        // Push character into an array
+        reversedString.unshift(char)
     }
-    // Read the character
-
-    // Push character into an array
-
     // Convert array toString
+    finalResult = reversedString.join('');
 
     // Return reverseString
-
+    return finalResult;
 };
 
 // Do not edit below this line
