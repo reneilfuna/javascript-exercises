ac02daa7dd83b144e4fa507f0eb12c8c5357fee1
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-12 15:26:45 +0000

    Implement code for 1st function to pass test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let conversion = (temperature - 32) * (5/9);
+  return Math.round(conversion*10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let conversion = (temperature * (9/5) + 32)
+  return Math.round(conversion*10) / 10
 };
 
 // Do not edit below this line
