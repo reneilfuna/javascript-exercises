4b8f36451e644e9b02ad0236147f348b91cd9539
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-12 14:10:45 +0000

    Implement code to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,24 @@
-const removeFromArray = function(array, toRemove) {
+const removeFromArray = function(array, ...toRemove) {
+
     let newArray = [];
-    // Check where item is in array
-    for (i=0; i<array.length; i++){
-        if (array[i] != toRemove)
-            newArray.push(array[i])
+    if (toRemove.length === 1){
+        // Check where item is in array
+        for (i=0; i<array.length; i++){
+            if (array[i] != toRemove)
+                newArray.push(array[i])
+        }
+        return newArray;
     }
-    console.log(newArray)
-    return newArray;
+    else if (toRemove.length > 1){
+        for (const remove of toRemove){
+            for (i=0; i<array.length; i++){
+                if(array[i] === remove){
+                    array.splice(i, 1)
+                }
+            }
+        }
+    return array;
+    }
 };
 
 // Do not edit below this line
