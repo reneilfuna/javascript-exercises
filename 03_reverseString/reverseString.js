63b6df571542cf10241f2f53db979f33ac9ad539
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-12 12:34:05 +0000

    Write pseudocode for exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    //Function takes a string and then reverses the string.
+
+    // Loop through each character in the string
+    for (i=0; i<string.length-1; i++) {
+        
+    }
+    // Read the character
+
+    // Push character into an array
+
+    // Convert array toString
+
+    // Return reverseString
 
 };
 
