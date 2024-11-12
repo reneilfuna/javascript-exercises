165bee6d8ebc069331701ac6da903898d559f66b
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-12 15:17:31 +0000

    Implement code to pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
 const leapYears = function(year) {
-    if( year%4 == 0 || year%400 == 0)
+    if (year%100 == 0 && year%400 !=0)
+        return false;
+    else if( year%4 == 0 || year%400 == 0)
         return true;
     else
         return false;
